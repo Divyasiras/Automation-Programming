16bcd432017e0402f5bdb0be3443148fe9508264
Divya Siras <divyashiras976@gmail.com>
2026-07-18 23:22:58 +0530

    Add files via upload
    This Python automation program uses the schedule library to execute a task automatically at regular intervals. The script defines a Display() function that prints the message "Jay Ganesh..!" along with the current date and time. The scheduler is configured to run this function every one minute, while an infinite loop continuously checks for pending scheduled tasks. This program demonstrates the basics of task scheduling and automation using Python and can be extended for reminders, monitoring, logging, and other automated applications.

---
+++ ScheduleDemo3.py
@@ -0,0 +1,19 @@
+import schedule
+import time
+import datetime
+
+def Display():
+    print("jay Ganesh..! ",datetime.datetime.now())  # show current time
+
+def main():
+    print("Automation print started")
+
+    schedule.every(1).minute.do(Display)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+    print("End of automation script")
+if __name__=="__main__":
+    main
